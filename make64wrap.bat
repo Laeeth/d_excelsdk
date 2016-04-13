c1b19916b6a70f794abdbd987658ea9b0805945b
Laeeth Isharc <laeeth@laeeth.com>
2016-04-13 22:16:08 +0100

    wrapping test

---
+++ make64wrap.bat
@@ -0,0 +1,3 @@
+rm generic64.xll
+dmd -c -gc -m64 -map -ofgeneric64.obj genericwithwrap.d  memorymanager.d memorypool.d xlcall.d xlcallcpp.d framework.d  wrap.d
+dmd -m64 -g -L/OUT:generic64.xll  -L/NOLOGO -L generic64.obj generic64.def xlcall64d.lib
